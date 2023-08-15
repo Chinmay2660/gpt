d60bc9b4f7834332dd54bdd619809dfd97767788
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-08-16 01:14:52 +0530

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -7,12 +7,24 @@ import {
   Possibility,
   WhatGPT3,
 } from "./containers";
-import { Brand, CTA, Feature, Navbar } from "./components";
+import { Brand, CTA, Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Brand />
+      <Footer />
     </div>
   );
 };
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3s</div>;
+  return <div>WhatGPT3</div>;
 };
 
 export default WhatGPT3;
