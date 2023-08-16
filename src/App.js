ee9c7e3c2fccb9fe88bcb2a978df7f05b7d60560
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-08-16 16:00:10 +0530

    Add Brands

--- src/App.js
+++ src/App.js
@@ -10,23 +10,20 @@ import {
 import { Brand, CTA, Navbar } from "./components";
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Brand />
-      <Footer />
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
